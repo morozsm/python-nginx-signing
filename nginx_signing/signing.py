dabcce3bbbd4818ac843002c2790ed9265994250
Sergey Morozik <morozsm@gmail.com>
2021-09-26 22:46:53 -0400

    Add python v3 support. Rename URL params to conventional

--- nginx_signing/signing.py
+++ nginx_signing/signing.py
@@ -1,23 +1,30 @@
+import sys
 from base64 import urlsafe_b64encode
 from hashlib import md5
 from time import time
-from urlparse import urlparse, urlunparse, ParseResult
+if (sys.version_info > (2, 7)):
+    PYTHON_VER = 3 if (sys.version_info >= (3, 0)) else 2
+    from urllib.parse import urlparse, urlunparse, ParseResult
+else:
+    PYTHON_VER = 2
+    from urlparse import urlparse, urlunparse, ParseResult
 
 DEFAULT = object()
 
 
 def generate_key(s):
-    return urlsafe_b64encode(md5(s).digest()).rstrip('=')
+    if PYTHON_VER == 2:
+        return urlsafe_b64encode(md5(s).digest()).rstrip('=')
+    else:
+        return urlsafe_b64encode(md5(s.encode('utf-8')).digest()).rstrip('='.encode('utf-8')).decode('utf-8')
 
 
 class Signer(object):
-    def __init__(self, key, timeout=DEFAULT, format='{key}{value}{expiration}'):
+    def __init__(self, key, timeout=DEFAULT, address = None, format='{key}{value}{expiration}'):
         self.key = key
-        if timeout is DEFAULT:
-            self.timeout = 60*60*24  # 24 hours
-        else:
-            self.timeout = timeout
-        self.format = format
+        self.timeout = 60*60*24 if timeout is DEFAULT else timeout
+        self.address = address
+        self.format = '{key}{address}{value}{expiration}' if address else format
 
     def sign(self, *args, **kwargs):
         raise NotImplementedError
@@ -29,9 +36,13 @@ class Nginx(Signer):
             return str(int(self.timeout+time()))
         return ''
 
-    def signature(self, s):
+    def signature(self, url):
         expiration = self.get_expiration()
-        string = self.format.format(key=self.key, value=s, expiration=expiration)
+        if not self.address:
+            string = self.format.format(key=self.key, value=url, expiration=expiration)
+        else:
+            # string = self.format.format(key=self.key, value=s, expiration=expiration, address=self.address)
+            string = f"{expiration}{url}{self.address} {self.key}"
         return generate_key(string), expiration
 
 
@@ -44,7 +55,7 @@ class UriSigner(Nginx):
         query = parsed.query
         if query:
             query += '&'
-        query += 'st=%s&e=%s' % (sig, exp)
+        query += 'md5=%s&expires=%s' % (sig, exp)
 
         return urlunparse(ParseResult(
             parsed.scheme, parsed.netloc,
@@ -54,4 +65,4 @@ class UriSigner(Nginx):
 class UriQuerySigner(Nginx):
     def sign(self, key, value):
         sig, exp = self.signature(value)
-        return '%s=%s&st=%s&e=%s' % (key, value, sig, exp)
+        return '%s=%s&md5=%s&expires=%s' % (key, value, sig, exp)
